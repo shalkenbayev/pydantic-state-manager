9580369ce9479f6fa09a5bc002f732d25e41e9e2
r.schalkenbajew <r.schalkenbajew@gmail.com>
2025-05-07 21:19:03 +0200

    Rename Redis URL env var and add empty conftest.py

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class StateManagerSettings(BaseSettings):
-    REDIS_URL: str = "redis://localhost:6379/0"
+    STATE_MANAGER_REDIS_URL: str = "redis://localhost:6379/0"
 
 
 state_manager_settings = StateManagerSettings()
